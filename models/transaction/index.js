94512c4acd4390102349c4f56d80516d0adac2a2
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-08-28 15:10:19 +0300

    added models, schemas, default avatar
    Added:
    models and schemas for transactions, categories and balance.
    Default avatar image

---
+++ models/transaction/index.js
@@ -0,0 +1,9 @@
+const transactionValidation = require("./transactionValidation");
+const transactionModel = require("./transactionModel");
+
+const transaction = {
+  transactionValidation,
+  transactionModel,
+};
+
+module.exports = transaction;
