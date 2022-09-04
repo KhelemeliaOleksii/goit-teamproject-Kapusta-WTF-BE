236f21e8242aa79bb9643c9a081a8438f783cf74
KhelemeliaOleksii <xvdm@ukr.net>
2022-09-04 14:30:29 +0300

    add errorHandler middleware and remove unused variables

--- models/user/userValidation.js
+++ models/user/userValidation.js
@@ -1,23 +1,23 @@
-const Joi = require("joi");
+const Joi = require('joi')
 
-const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
 
 const signupSchema = Joi.object({
   // username: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
-});
+  password: Joi.string().min(6).required()
+})
 
 const emailSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required(),
-});
+  email: Joi.string().pattern(emailRegexp).required()
+})
 
-const validateSchema = (user) => signupSchema.validate(user);
-const validateEmail = (email) => emailSchema.validate(email);
+const validateSchema = (user) => signupSchema.validate(user)
+const validateEmail = (email) => emailSchema.validate(email)
 
 const userValidation = {
   validateSchema,
-  validateEmail,
-};
+  validateEmail
+}
 
-module.exports = userValidation;
+module.exports = userValidation
