65c62174639220dc422c7fd3cda775d7bc74156c
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-09-06 23:20:09 +0300

    commit

--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -4,11 +4,10 @@ const { userModel } = require('../../models/user/')
 
 const verifyEmail = asyncHandler(async (req, res) => {
   const { verificationToken } = req.params
-  console.log(verificationToken);
   const user = await userModel.findOne({ verificationToken })
   if (!user) {
-    res.status(404).redirect("https://wtf-kapusta.netlify.app/login")
-    throw new Error('User not found')
+    res.status(404)
+    throw new Error('User authorizated allready')
   }
   await userModel.findByIdAndUpdate(user._id, {
     verificationToken: '',
