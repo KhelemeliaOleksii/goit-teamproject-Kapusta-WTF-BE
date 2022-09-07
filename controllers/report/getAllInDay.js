9a8ad2d5fc6dc53d9dc221bbf0b1aed5b4a64a1b
Vasyl Hrytsyshyn <matsjo19@gmail.com>
2022-09-07 16:06:13 +0300

    test 03

--- controllers/report/getAllInDay.js
+++ controllers/report/getAllInDay.js
@@ -2,11 +2,15 @@ const asyncHandler = require('express-async-handler')
 const reportServises = require('../../services/report')
 
 const controller = asyncHandler(async (req, res) => {
-  const { _id: userId } = req.user
-  if (!userId) {
+  const { _id } = req.user
+
+  if (!_id) {
     res.status(401)
     throw new Error('Not authorized')
   }
+
+  const userId = _id.toString()
+
   const { date } = req.query
   if (!date) {
     res.status(400)
