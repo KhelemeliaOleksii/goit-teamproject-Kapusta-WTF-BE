78252121069a7f03f6f55531bc8c391f6ed12b2c
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-09-09 21:16:00 +0300

    fix-auth-middleware

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -7,26 +7,41 @@ const { SECRET_KEY } = process.env
 const auth = async (req, res, next) => {
   const { authorization } = req.headers
   if (!authorization) {
-    res.status(401)
-    throw new Error('Not authorized')
+    res.status(401).json({
+      status: 'error',
+      code: 401,
+      message: 'Not authorized',
+    })
+    return
   }
   const [bearer, token] = authorization.split(' ')
   if (bearer !== 'Bearer') {
-    res.status(401)
-    throw new Error('Not authorized')
+    res.status(401).json({
+      status: 'error',
+      code: 401,
+      message: 'Not authorized',
+    })
+    return
   }
   try {
     const { _id } = jwt.verify(token, SECRET_KEY)
     const user = await userModel.findById(_id)
     if (!user.token) {
-      res.status(401)
-      throw new Error('Not authorized')
+      res.status(401).json({
+        status: 'error',
+        code: 401,
+        message: 'Not authorized',
+      })
+      return
     }
     req.user = user
     next()
   } catch (error) {
-      res.status(401)
-      throw new Error('Not authorized')
+    res.status(401).json({
+      status: 'error',
+      code: 401,
+      message: 'Not authorized',
+    })
   }
 }
 
