372d691a3a81962a66bf3203ed28f9f251f89ea7
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-09-09 22:51:13 +0300

    return auth

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -6,24 +6,25 @@ const { userModel } = require('../../models/user')
 const { SECRET_KEY } = process.env
 
 const auth = asyncHandler(async (req, res, next) => {
-  const { authorization } = req.headers
-  if (!authorization) {
-    res.status(401)
-    throw new Error('Not authorized')
-  }
+  const { authorization = "" } = req.headers
   const [bearer, token] = authorization.split(' ')
   if (bearer !== 'Bearer') {
     res.status(401)
     throw new Error('Not authorized')
   }
-  const { _id } = jwt.verify(token, SECRET_KEY)
-  const user = await userModel.findById(_id)
-  if (!user.token) {
+  try {
+   const { id } = jwt.verify(token, SECRET_KEY)
+   const user = await userModel.findById(id)
+   if (!user || !user.token) {
     res.status(401)
     throw new Error('Not authorized')
     }
-  req.user = user
-  next()
+    req.user = user
+    next()
+  } catch (error) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
 })
 
 module.exports = auth
