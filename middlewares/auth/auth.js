a4c9eb34b581b1deb9c3e64a028d51de11ed7b36
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-09-09 21:26:37 +0300

    secand fix auth middleware

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
 
 const { userModel } = require('../../models/user')
 
 const { SECRET_KEY } = process.env
 
-const auth = async (req, res, next) => {
+const auth = asyncHandler(async (req, res, next) => {
   const { authorization } = req.headers
   if (!authorization) {
     res.status(401)
@@ -15,19 +16,14 @@ const auth = async (req, res, next) => {
     res.status(401)
     throw new Error('Not authorized')
   }
-  try {
-    const { _id } = jwt.verify(token, SECRET_KEY)
-    const user = await userModel.findById(_id)
-    if (!user.token) {
-      res.status(401)
-      throw new Error('Not authorized')
+  const { _id } = jwt.verify(token, SECRET_KEY)
+  const user = await userModel.findById(_id)
+  if (!user.token) {
+    res.status(401)
+    throw new Error('Not authorized')
     }
-    req.user = user
-    next()
-  } catch (error) {
-      res.status(401)
-      throw new Error('Not authorized')
-  }
-}
+  req.user = user
+  next()
+})
 
 module.exports = auth
